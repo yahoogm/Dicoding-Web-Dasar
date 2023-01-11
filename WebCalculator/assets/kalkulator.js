d800a240cf73010d956bfcf7b4ea4176b021874b
yahoogm <yaho.garfield@gmail.com>
2023-01-11 21:52:19 +0700

    add variable history

--- WebCalculator/assets/kalkulator.js
+++ WebCalculator/assets/kalkulator.js
@@ -57,7 +57,15 @@ function performCalculation() {
     result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
   }
 
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result,
+  };
+  putHistory(history);
   calculator.displayNumber = result;
+  renderHistory();
 }
 
 const buttons = document.querySelectorAll('.button');
