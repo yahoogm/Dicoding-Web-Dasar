a7d2589060af04efe5d1f38061df6981d77d70a0
yahoogm <yaho.garfield@gmail.com>
2023-01-12 13:46:42 +0700

    add interaction to html

---
+++ WebProfile/main.js
@@ -0,0 +1,6 @@
+console.log('You did it!');
+
+document.getElementsByClassName('banner').innerHTML = 'Definition';
+document.getElementsByClassName('banner').innerHTML = 'Features';
+document.getElementsByClassName('banner').innerHTML = 'Benefits';
+document.getElementsByClassName('flex-container').innerHTML = 'Samuel Abelino Simanullang';
