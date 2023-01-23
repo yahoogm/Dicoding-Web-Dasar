50505d31eda7e78aa3c1d3d804f2106b150fa9d0
yahoogm <yaho.garfield@gmail.com>
2023-01-23 16:10:50 +0700

    change var to const

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,33 +1,29 @@
-var main = function () {
-  var searchElement = document.querySelector('#searchElement');
-  var buttonSearchElement = document.querySelector('#searchButtonElement');
-  var clubListElement = document.querySelector('#clubList');
+const main = function () {
+  const searchElement = document.querySelector('#searchElement');
+  const buttonSearchElement = document.querySelector('#searchButtonElement');
+  const clubListElement = document.querySelector('#clubList');
 
-  var onButtonSearchClicked = function () {
-    var dataSource = new DataSource(renderResult, fallbackResult);
+  const onButtonSearchClicked = function () {
+    const dataSource = new DataSource(renderResult, fallbackResult);
     dataSource.searchClub(searchElement.value);
   };
 
-  var renderResult = function (results) {
+  const renderResult = function (results) {
     clubListElement.innerHTML = '';
     results.forEach(function (club) {
-      var name = club.name;
-      var fanArt = club.fanArt;
-      var description = club.description;
+      const name = club.name;
+      const fanArt = club.fanArt;
+      const description = club.description;
 
-      var clubElement = document.createElement('div');
+      const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
 
-      clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-          '<div class="club-info">\n' +
-          '<h2>' + name + '</h2>\n' +
-          '<p>' + description + '</p>' +
-          '</div>';
+      clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' + '<div class="club-info">\n' + '<h2>' + name + '</h2>\n' + '<p>' + description + '</p>' + '</div>';
       clubListElement.appendChild(clubElement);
     });
   };
 
-  var fallbackResult = function (message) {
+  const fallbackResult = function (message) {
     clubListElement.innerHTML = '';
     clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>';
   };
