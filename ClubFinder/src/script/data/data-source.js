c10974ea77e557564f58fa47d689714ed76004e8
yahoogm <yaho.garfield@gmail.com>
2023-01-25 15:38:09 +0700

    change to async await from callback

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,16 +1,13 @@
 class DataSource {
-  constructor(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-  }
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter((club) => club.name.toUpperCase().includes(keyword.toUpperCase()));
 
-  searchClub(keyword) {
-    const filteredClubs = clubs.filter((club) => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-    if (filteredClubs.length) {
-      this.onSuccess(filteredClubs);
-    } else {
-      this.onFailed(`${keyword} is not found`);
-    }
+      if (filteredClubs.length) {
+        resolve(filteredClubs);
+      } else {
+        reject(`${keyword} is not found`);
+      }
+    });
   }
 }
