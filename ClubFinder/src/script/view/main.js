a508a9eaf171088cb5d1f9f6567cb80b30ddb86a
yahoogm <yaho.garfield@gmail.com>
2023-01-31 16:08:19 +0700

    - change value in variable clubList - change logic in variable renderResult - change logic in variable fallbackResult

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,8 +1,10 @@
 import DataSource from '../data/data-source.js';
+import '../component/club-list.js';
+import '../component/search-bar.js';
 
 const main = () => {
   const searchElement = document.querySelector('search-bar');
-  const clubListElement = document.querySelector('#clubList');
+  const clubListElement = document.querySelector('club-list');
 
   const onButtonSearchClicked = async () => {
     try {
@@ -14,26 +16,11 @@ const main = () => {
   };
 
   const renderResult = (results) => {
-    clubListElement.innerHTML = '';
-    results.forEach((club) => {
-      const { name, fanArt, description } = club;
-      const clubElement = document.createElement('div');
-      clubElement.setAttribute('class', 'club');
-
-      clubElement.innerHTML = `
-        <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-        <div class="club-info">
-         <h2>${name}</h2>
-         <p>${description}</p>
-        </div>`;
-
-      clubListElement.appendChild(clubElement);
-    });
+    clubListElement.clubs = results;
   };
 
   const fallbackResult = (message) => {
-    clubListElement.innerHTML = '';
-    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    clubListElement.renderError(message);
   };
 
   searchElement.clickEvent = onButtonSearchClicked;
