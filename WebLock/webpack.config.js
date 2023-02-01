638a248f8c341bd39fb46660930aacea26f08e77
yahoogm <yaho.garfield@gmail.com>
2023-02-01 20:47:38 +0700

    add babel loader

--- WebLock/webpack.config.js
+++ WebLock/webpack.config.js
@@ -8,4 +8,34 @@ module.exports = {
   },
 
   mode: 'production',
+  module: {
+    rules: [
+      // style and css loader
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+
+      // babel loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              preset: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
