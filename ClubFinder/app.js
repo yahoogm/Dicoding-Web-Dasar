ae671bc20a0a986649f4c292109375d070c9b29e
yahoogm <yaho.garfield@gmail.com>
2023-02-02 10:53:24 +0700

    add webpack

--- ClubFinder/app.js
+++
@@ -1,5 +0,0 @@
-import main from './src/script/view/main.js';
-import './src/script/component/app-bar.js';
-import './src/script/component/search-bar.js';
-
-document.addEventListener('DOMContentLoaded', main);
---
+++ ClubFinder/src/app.js
@@ -0,0 +1,7 @@
+import main from './script/view/main.js';
+import './script/component/app-bar.js';
+import './script/component/search-bar.js';
+import './styles/style.css';
+import 'regenerator-runtime';
+
+document.addEventListener('DOMContentLoaded', main);
