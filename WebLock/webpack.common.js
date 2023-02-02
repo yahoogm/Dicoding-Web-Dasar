9bb187d10c32dd12ab8448c48e2102690946a2b5
yahoogm <yaho.garfield@gmail.com>
2023-02-02 10:22:12 +0700

    add script to run dev & production

---
+++ WebLock/webpack.common.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      /* style and css loader */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+    }),
+  ],
+};
