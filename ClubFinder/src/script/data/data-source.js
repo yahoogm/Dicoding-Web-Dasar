e69e4dcbcad6ba0e9b323a3493062ceeb4381921
yahoogm <yaho.garfield@gmail.com>
2023-02-03 13:32:50 +0700

    refactoring code to implemen fetch

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,15 +1,13 @@
-import clubs from './clubs.js';
-
 class DataSource {
   static searchClub(keyword) {
-    return new Promise((resolve, reject) => {
-      const filteredClubs = clubs.filter((club) => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-      if (filteredClubs.length) {
-        resolve(filteredClubs);
-      } else {
-        reject(`${keyword} is not found`);
-      }
+    return fetch(`https://sports-api.dicoding.dev/teams/search?t=${keyword}`).then((response) => {
+      return response.json().then((responseJson) => {
+        if (responseJson.teams) {
+          return Promise.resolve(responseJson.teams);
+        } else {
+          return Promise.reject(`${keyword} is not found`);
+        }
+      });
     });
   }
 }
